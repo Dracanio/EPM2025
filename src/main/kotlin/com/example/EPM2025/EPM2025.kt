513b507574303686c902bf9333c46efc5f32b29a
Andre Steurer <amdre.steurer@smail.th-koeln.de>
2026-01-14 19:04:46 +0100

    Initialized Spring

---
+++ src/main/kotlin/com/example/EPM2025/EPM2025.kt
@@ -0,0 +1,11 @@
+package com.example.EPM2025
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EPM2025
+
+fun main(args: Array<String>) {
+    runApplication<EPM2025>(*args)
+}
---
+++ src/test/kotlin/com/example/EPM2025/EPM2025Tests.kt
@@ -0,0 +1,13 @@
+package com.example.EPM2025
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EPM2025Tests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
